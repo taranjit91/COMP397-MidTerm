3ef5bbe1f155e1702e7a705407f788c7e7bc62d8
Taranjit Kaur <parmar.taran@gmail.com>
2017-12-16 15:54:07 -0500

    conflict merged for reset function

--- Scripts/objects/plane.ts
+++ Scripts/objects/plane.ts
@@ -45,13 +45,11 @@ module objects {
       this.bulletSpawn.y = this.y - 35;
       this._checkBounds();
     }
-<<<<<<< HEAD
-    public Reset():void{
-=======
+
 
     public Reset():void
     {
->>>>>>> fcfd6c6f2265b2739f05ed89b8b700b62174d6fb
+
       
     }
   }
